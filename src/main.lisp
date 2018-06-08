3692372e017a8dad8df9556b2d5c86f319fefd48
Alexandre Cesar <acmorcego@gmail.com>
2018-06-08 15:39:31 -0300

    assembly main

--- src/main.lisp
+++ src/main.lisp
@@ -1,2 +1,27 @@
+(load "line.lisp")
+
+(include "distance.h")
+(include "io.h")
+(include "tree.h")
+(include <stddef.h>)
+(include <stdlib.h>)
+(include <string.h>)
+
 (function main () -> int
+	(decl ((uint16_t dictionaryLength = (readNumber))
+				 (uint16_t changes = (readNumber))
+				 (char* query = (readWord))))
+
+	(line)
+	(decl ((Node* tree = NULL)))
+	(for ((int i = 0) (< i dictionaryLength) i++)
+		(decl ((char* text = (readWord))
+					 (uint16_t cost = (distance text (strlen text) query (strlen query)))))
+		(if (<= cost changes)
+			(set tree (insert tree cost text))
+			(free text)))
+	(free query)
+
+	(line)
+	(traverse tree)
 	(return 0))
