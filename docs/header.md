a5b7c656dee145942129604b157a557fc7fc2852
Alexandre Cesar <acmorcego@gmail.com>
2018-06-08 19:33:50 -0300

    add header

---
+++ docs/header.md
@@ -0,0 +1,3 @@
+# AEDS III - TP2 - Sugestões Doodle
+## Alexandre Cesar da Silva
+## Junho de 2018
--- docs/index.js
+++ docs/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import 'katex/dist/katex.min.css'
+import Header from './header.md'
 
-render(<div />, document.querySelector('article'))
+render(<Header />, document.querySelector('article'))
