ebbe1c234ba6bba6aeb4ee71441a9739b3e31fe7
Alexandre Cesar <acmorcego@gmail.com>
2018-06-09 19:46:01 -0300

    write experiments

--- docs/experiments.md
+++ docs/experiments.md
@@ -1,1 +1,28 @@
+import {
+	VictoryAxis,
+	VictoryChart,
+	VictoryContainer,
+	VictoryLabel,
+	VictoryLine
+} from 'victory'
+import { InlineMath } from 'react-katex'
+
+import { time } from './snippets.js'
+
 ## Análise Experimental
+
+Para cada caso de teste disponibilizado, o programa foi executado 10 vezes e o tempo médio
+coletado. No gráfico abaixo é mostrado a relação de tempo por <InlineMath math="m*n"/>
+(número de caracteres no dicionário * número de caracteres na busca). E de fato vemos que
+a proporção seque um padrão linear.
+
+<VictoryChart containerComponent={<VictoryContainer responsive={false}/>}>
+	<VictoryLine data={time} x="value" y="avg" />
+	<VictoryAxis label="m * n" tickCount={4} />
+	<VictoryAxis
+		dependentAxis
+		label="Time (s)"
+		axisLabelComponent={<VictoryLabel dx={-60}/>}
+		tickCount={2}
+		tickFormat={t => (t/1000).toFixed(1)} />
+</VictoryChart>
--- docs/snippets.js
+++ docs/snippets.js
@@ -1,5 +1,4 @@
-export const levenshtein =
-`levenshtein(text, textLength, query, queryLength) {
+export const levenshtein = `levenshtein(text, textLength, query, queryLength) {
 	if (textLength == 0) return queryLength
 	if (queryLength == 0) return textLength
 	if (text[textLength - 1] == query[queryLength - 1]) cost = 0
@@ -9,8 +8,7 @@ export const levenshtein =
 	           levenshtein(text, textLength - 1, query, queryLength - 1) + cost)
 }`
 
-export const memoization =
-`levenshtein(text, textLength, query, queryLength) {
+export const memoization = `levenshtein(text, textLength, query, queryLength) {
 	if (memory[textLength][queryLength].calculated)
 		return memory[textLength][queryLength].value
 	// process
@@ -18,8 +16,7 @@ export const memoization =
 	return result
 }`
 
-export const tree =
-`insert(node, value) {
+export const tree = `insert(node, value) {
 	if (node.empty) return Node(value)
 	if (value < node.value) node.left = insert(node.left, value)
 	else node.right = insert(node.right, value)
@@ -35,3 +32,16 @@ traverse(node) {
 
 export const sum = `m = \\sum_i len(palavra_i)`
 export const complexity = `\\theta(mn)`
+
+export const time = [
+  { value: 1026, avg: 1.7 },
+  { value: 9399, avg: 2 },
+  { value: 56796, avg: 3.5 },
+  { value: 2313288, avg: 48.8 },
+  { value: 81955923, avg: 1855 },
+  { value: 111208929, avg: 2525 },
+  { value: 144306692, avg: 3027 },
+  { value: 826563262, avg: 20166 },
+  { value: 1267912690, avg: 34196 },
+  { value: 1559053090, avg: 40477 }
+]
